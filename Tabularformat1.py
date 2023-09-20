fef5ad3bc032c235f00428c6d8ba4b9af64720f9
anurout <anusha.rout@capgemini.com>
2023-09-20 20:44:22 +0530

    added banner

--- Tabularformat1.py
+++ Tabularformat1.py
@@ -167,6 +167,6 @@ li {{
         with open(html_file_path, 'w') as html_file:
             html_file.write(html_output)
 
-        print(f"HTML report generated: {html_file_path}")#print
+        print(f"HTML report generated: {html_file_path}")#p
     except json.JSONDecodeError:
         print(f"Error decoding JSON in {json_file_path}")
